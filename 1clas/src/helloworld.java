b287693743d977fdfeec6c4d2d4456be1a6b4442
ELCOT <ELCOT@Lenovo>
2022-02-05 01:14:26 -0800

    my first commit

---
+++ 1clas/src/helloworld.java
@@ -0,0 +1,18 @@
+
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world....");
+	}
+
+}
+
+
+/*
+1. In this program you understood how to create a project,how to create a class and how to excute and print 
+"hellow world " on the console
+*/
